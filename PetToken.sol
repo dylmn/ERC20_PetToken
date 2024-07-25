ddbeeb108fab71d24f84800dc7f3434c5cdd814d
dylmn <67588622+dylmn@users.noreply.github.com>
2024-07-26 02:15:44 +0800

    Create PetToken.sol

---
+++ PetToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PetToken is ERC20, Ownable {
+    constructor(address initialOwner) ERC20("PetToken", "PT") Ownable(initialOwner) {}
+
+    function mintPetToken(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burnPetToken(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
